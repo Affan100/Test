287e4d4e5e9969ffd5e04ecacb5a451ccd390828
Affan100 <affan_pathan@hotmail.com>
2019-10-30 20:52:19 +0700

    done

---
+++ node.js
@@ -0,0 +1,20 @@
+// let x= 10
+// x= 'c',"c"
+// x= 'xcx'
+// x = `sdf` //alt +96
+// let arr = [2 ,3, 6]
+// let obj = {
+//     a : 2,
+//     b:3
+// }
+// ActiveXObject['a']
+// obj.a
+
+function fn(a, b){
+    return a +  b;
+}
+const fn2 = (a, b) => {
+    return a -  b;
+}
+
+console.log(fn(10, 20)) 
